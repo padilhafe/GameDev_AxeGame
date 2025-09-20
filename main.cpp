0a06928d96c014b9de9481174e5361169ae246bb
Felipe Duellis Padilha <felipedpadilha@gmail.com>
2025-09-20 15:45:48 -0300

    feat: Colision detection and GameOver logic.
    - Added Edge Values Update in each frame to check for colision;
    - Added colision detection;
    - Added gameover and restart logic;

--- main.cpp
+++ main.cpp
@@ -19,10 +19,10 @@ int main()
     int playerRadius = 50;
     int playerSpeed = 15;
     // Player Edges
-    int playerEdgeLeft = playerX - playerRadius;
-    int playerEdgeRigth = playerX + playerRadius;
-    int playerEdgeTop = playerY - playerRadius;
-    int playerEdgeBottom = playerY + playerRadius;
+    int playerXEdgeLeft = playerX - playerRadius;
+    int playerXEdgeRight = playerX + playerRadius;
+    int playerYEdgeTop = playerY - playerRadius;
+    int playerYEdgeBottom = playerY + playerRadius;
 
     // Enemy Configuration
     int enemyWidth = 50;
@@ -31,10 +31,10 @@ int main()
     int enemyX;
     int enemyY = -enemyHeight;
     // Enemy Edges
-    int enemyEdgeLeft = enemyX;
-    int enemyEdgeRigth = enemyX + enemyWidth;
-    int enemyEdgeTop = enemyY;
-    int enemyEdgeBottom = enemyY + enemyHeight;
+    int enemyXEdgeLeft = enemyX;
+    int enemyXEdgeRight = enemyX + enemyWidth;
+    int enemyYEdgeTop = enemyY;
+    int enemyYEdgeBottom = enemyY + enemyHeight;
 
     // Other Game Configurations
     bool gameOver = false;
@@ -57,8 +57,20 @@ int main()
         {
             DrawText("Game Over!", screenWidth / 2 - MeasureText("Game Over!", 50) / 2, screenHeight / 2 - 25, 50, RED);
             DrawText(TextFormat("Final Score: %04d", score), screenWidth / 2 - MeasureText(TextFormat("Final Score: %04d", score), 30) / 2, screenHeight / 2 + 40, 30, BLACK);
+            DrawText("Press R or (Start) to Restart", screenWidth / 2 - MeasureText("Press ENTER or (A) to Restart", 20) / 2, screenHeight / 2 + 80, 20, DARKGRAY);
+
+            // Restart Game Logic
+            if ((IsKeyPressed(KEY_R) || 
+                IsGamepadButtonPressed(GAMEPAD, GAMEPAD_BUTTON_MIDDLE_RIGHT)))
+            {
+                score = 0;
+                enemyY = -enemyHeight;
+                enemyX = GetRandomValue(enemyWidth, screenWidth - enemyWidth);
+                playerX = screenWidth / 2;
+                playerY = screenHeight - 100;
+                gameOver = false;
+            }
         } else {
-            // Game Logic Begins
             // Player Spawn
             DrawCircle(playerX, playerY, playerRadius, BLUE);
             // Enemy Spawn
@@ -69,10 +81,33 @@ int main()
             if (enemyY > screenHeight + enemyHeight || enemyY < -enemyHeight)
             {
                 score++;
+                DrawRectangle(enemyX, enemyY, enemyWidth, enemyHeight, RED);
 
                 // Reverse the enemy movement position
                 // to do: bounce towards the player
                 enemySpeed = -enemySpeed;
+            }
+
+            // Updates Edges Positions
+            // Player Edges
+            playerXEdgeLeft = playerX - playerRadius;
+            playerXEdgeRight = playerX + playerRadius;
+            playerYEdgeTop = playerY - playerRadius;
+            playerYEdgeBottom = playerY + playerRadius;
+            
+            // Enemy Edges
+            enemyXEdgeLeft = enemyX;
+            enemyXEdgeRight = enemyX + enemyWidth;
+            enemyYEdgeTop = enemyY;
+            enemyYEdgeBottom = enemyY + enemyHeight;
+
+            // Check for collision
+            if ((enemyXEdgeRight >= playerXEdgeLeft) &&
+                (enemyXEdgeLeft <= playerXEdgeRight) &&
+                (enemyYEdgeBottom >= playerYEdgeTop) &&
+                (enemyYEdgeTop <= playerYEdgeBottom))
+            {
+                gameOver = true;
             }
 
             // Player Movement Controls
